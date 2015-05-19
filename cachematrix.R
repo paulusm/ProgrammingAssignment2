02233c971a189b68d60c10999af5b6f7658d47e1
Paulus <paulusm@gmail.com>
2015-05-19 22:23:26 +0100

    Commit the work

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Closure created with a specific matrix argument 
+## Returns accessor functions for the
+## matrix argument and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  # variable to hold the inverse
+  mtrx <- NULL
+  
+  # setter - initialise the argument matrix and reset the inverse
+  set <- function(y) {
+    x <<- y
+    mtrx <<- NULL
+  }
+  # getter - return the argument matrix
+  get <- function() x
+  
+  #setter - set the inverse
+  setinverse <- function(inv) mtrx <<- inv
+  
+  #getter - get the inverse
+  getinverse <- function() mtrx
+  
+  #return the getter and setter functions
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function operates on the closure 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # see if inverse is in the cache
+  mtrx <- x$getinverse()
+  
+  # If the inverse isavailable we return it and exit.
+  if(!is.null(mtrx)) {
+    message("getting cached data")
+    return(mtrx)
+  }
+  
+  # Otherwise we get the data, calculate the inverse and store that back in the
+  # closure. Return the newly calculated inverse
+  data <- x$get()
+  mtrx <- solve(data, ...)
+  x$setinverse(mtrx)
+  mtrx
 }
